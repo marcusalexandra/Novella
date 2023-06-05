ceca80bb372a2e39a8dee53696fd7edc3330d62e
marcusalexandra <marcusalexandra2@gmail.com>
2023-06-05 22:55:03 +0300

    Second Commit

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = mysqli_connect('localhost', 'root', '');
-if ( $db === FALSE ) die('Fail message');
-if ( mysqli_select_db($db, "novelladb") === FALSE ) die("Fail message");
-?>  
--- index.php
+++ index.php
@@ -1,58 +1,56 @@
+<?php
+include 'conn.php';
+error_reporting(0);
+session_start();
+
+
+if(isset($_POST['submit'])) {
+
+  $email = $_POST['email'];
+  $pass = md5($_POST['pass']);
+  $sql = "SELECT * FROM users WHERE email = '$email' AND user_password = '$pass'";
+  $result = mysqli_query($connect,$sql);
+
+  if($result -> num_rows > 0) {
+    $sql = "SELECT user_id FROM users WHERE email = '$email' ";
+    $result = mysqli_query($connect, $sql);
+    while($row = $result -> fetch_assoc()) {
+      $user_id = $row["user_id"];
+    }
+    $_SESSION['user_id'] = $user_id;
+    header("Location: links/home.php");
+  }
+  else {
+    echo "User or password is wrong!";
+  }
+
+}
+?>
 <!DOCTYPE html>
-<html>
-
-	<head>
-		<meta charset="UTF-8">
-		<title>Novella - Homepage</title>
-		<link rel="stylesheet" type="text/css" href="Assets/CSS/site.css">
-		<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
-	</head>
-
-  <body>
-	<header class="header">
-		<a href="index.php" class="logo">
-			<img id="logo" src="Assets/Images/novella_logo.png" alt="Combs 'n' Fro Logo"/>
-		</a>
-		
-		<nav class="header-right">
-			<a href="index.php">Home</a>
-			<a href="account.php">My Novella Account</a>
-			<a href="search.php">Search Book Catelogue</a>
-  		</nav>
-	</header>
-
-	<section id="centerArea">
-		<section class="Content">
-			<h2>Welcome to Novella</h2>
-			<p>This is a book reservation website where you can search our database for books and reserve them </p>
-			<br>
-			<?php
-			session_start(); 
-			
-			if(!empty($_SESSION['username'])) 
-			{ 
-				echo "<div>";
-				echo "	<p>Sign out of your Novella account</p>";
-				echo '	<a class="btn btn-alt" href="signout.php">Sign Out</a>';
-				echo "</div>";
-			}
-			else
-			{
-				echo "	<p>Login to your account to avail of all services provided by Novella</p>";
-				echo '	<a class="btn btn-alt" href="login.html">Sign In</a>';
-				
-				echo "<br>";
-				
-				echo "	<p>Don't have an account? Click the register button below to create an account</p>";
-				echo '<a class="btn btn-alt" href="register.php">Register</a>';
-			}
-			?>
-		</section>
-	</section>
-
-		<footer class="footer">
-			<p>Website Created by Djibril Coulybaly - C18423664 &copy 2019.</p>
-    </footer>
-	</body>
-
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <title>Log In</title>
+</head>
+<body>
+  <br>
+  <br>
+  <div class="container">
+    <form action="" method="POST" class="login-email">
+      <div class="input-group">
+        <input type="email" placeholder="Email" name="email" value="<?php echo $email ?>" required>
+      </div>
+      <div class="input-group">
+        <input type="password" placeholder="Password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"   required title="Password must be 8 characters including 1 uppercase letter, 1 lowercase letter and numeric characters" name="pass" value="<?php echo $pass ?>" required>
+      </div>
+      <div class="input-group">
+        <button name="submit" class="btn">Sign Up</button>
+      </div>
+      <p class="login-register-text" style = "text-align:center;">Don't have an account? <a href="login/signup.php">Sign Up Here</a>.</p>
+    </form>
+  </div>
+</body>
 </html>
